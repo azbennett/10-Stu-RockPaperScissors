8379396f22a58c04f7d6dfc0f70fa776371bfdeb
Steve Bennett <128557327+azbennett@users.noreply.github.com>
2023-04-09 13:33:17 -0700

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,7 @@
 def main(): 
     import random
     import os
+    import time
 
     os.system('cls')  #cleans things up so its nice and tidy
 
@@ -15,17 +16,20 @@ def main():
     computer_choice = random.choice(options)
 
     #The human picks their response
-    user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
-
+    correct_choices = ["r", "rock", "p", "paper", "s", "scissor", "scissors"]
+    user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ").lower()
+    
     rock = "Rock"
     paper = "Paper"
-    scissor = "Scissor"
+    scissor = "Scissors"
 
     #Now it is time to clean up the responses for both the PC and NPC
-    if user_choice == "r": answer = rock
-    elif user_choice == "p": answer = paper
-    elif user_choice == "s": answer = scissor
+    if user_choice in correct_choices:
+        if (user_choice == "r") or (user_choice == "rock"): answer = rock
+        elif (user_choice == "p") or (user_choice == "paper"): answer = paper
+        elif (user_choice == "s") or (user_choice == "scissor") or (user_choice == "scissors"): answer = scissor
     else:
+        time.sleep(.5)
         print()
         print("Uh-oh, looks like you did not answer correctly.  Lets try this again.")
         input("Press any key to continue...")
@@ -36,28 +40,35 @@ def main():
     else: c_answer = scissor
 
     #compare NPC VS PC
-    if (computer_choice == user_choice):         #checks for a tie
+    if (computer_choice == user_choice[0]):         #checks for a tie
+        time.sleep(.5)
         print()
         print(f"You picked the same thing as the computer! {answer} vs {c_answer} was a tie.")
-    elif (computer_choice == "r") and (user_choice == "p"):   
+    elif (computer_choice == "r") and ((user_choice == "p") or (user_choice == "paper")):   
+        time.sleep(.5)
         print()
         print(f"Your {answer} beat {c_answer}!")  #paper beats rock        
-    elif (computer_choice == "p") and (user_choice == "s"): 
+    elif (computer_choice == "p") and ((user_choice == "s") or (user_choice == "scissor") or (user_choice == "scissors")): 
+        time.sleep(.5)
         print()
         print(f"Your {answer} beat {c_answer}!")  #scissors beats paper
-    elif (computer_choice == "s") and (user_choice == "r"): 
+    elif (computer_choice == "s") and ((user_choice == "r") or (user_choice == "rock")): 
+        time.sleep(.5)
         print()
         print(f"Your {answer} beat {c_answer}!")  #rock beats scissors
     else:                                                       #everything else loses
+        time.sleep(.5)
         print()
         print(f"Ohh, better luck next time.  Your {answer} was defeated by the {c_answer}.")
     
+    time.sleep(.5)
     print()
     restart = input("Would you like to go again? (y/n)")    #play again?
     if restart == "y":                                      #if so it starts again
         main()
     else:  
-        print()                                                 #if not it exits
+        time.sleep(.5)
+        print()                                             #if not it exits
         print("Thanks for playing!")
         exit()
 
